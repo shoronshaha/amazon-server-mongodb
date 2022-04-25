34c18ed3792bbfa617bdb4d67791dadf8aa09766
shoronshaha <smaranshaha@gmail.com>
2022-04-26 02:11:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saj0i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try { }
+    finally { }
+
+}
+
+
+app.get('/', (req, res) => {
+    res.send('jhon is running and waiting for ema');
+});
+
+app.listen(port, () => {
+    console.log('john is running on port', port);
+})
